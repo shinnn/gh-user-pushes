5c065af426265f8f5b732963e04837fdb42ac5f2
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-10-25 10:46:59 +0900

    avoid function recreation per call

--- index.js
+++ index.js
@@ -7,15 +7,19 @@
 const arrFilter = require('arr-filter');
 const ghUserEvents = require('gh-user-events');
 
+function filterPushes(event) {
+  return event.type === 'PushEvent';
+}
+
+function excludeNonPushEvents(events) {
+  return Promise.resolve(arrFilter(events, filterPushes));
+}
+
 module.exports = function ghUserPushes(user, options) {
   options = options || {};
   options.headers = Object.assign({
     'user-agent': 'https://github.com/shinnn/github-user-push-events'
   }, options.headers);
 
-  return ghUserEvents(user, options).then(function excludeNonPushEvents(events) {
-    return Promise.resolve(arrFilter(events, function filterPushes(event) {
-      return event.type === 'PushEvent';
-    }));
-  });
+  return ghUserEvents(user, options).then(excludeNonPushEvents);
 };
